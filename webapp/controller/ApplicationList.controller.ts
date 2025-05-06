677cc93533128bbab706e1c76cca96a1f2f58501
Nang Nguyen <nanggl19092001@gmail.com>
2025-05-06 09:16:30 +0000

    implement with CAP api v4

--- webapp/controller/ApplicationList.controller.ts
+++ webapp/controller/ApplicationList.controller.ts
@@ -1,15 +1,17 @@
 import MessageToast from "sap/m/MessageToast";
 import { Tile$PressEvent } from "sap/m/Tile";
-import Event from "sap/ui/base/Event";
 import Controller from "sap/ui/core/mvc/Controller";
 import UIComponent from "sap/ui/core/UIComponent";
 
 export default class ApplicationList extends Controller {
-    onPress(oEvent: Tile$PressEvent){
-        let sDestination = this.getView()?.getLocalId(oEvent.getSource().getId());
+    onPress(oEvent: Tile$PressEvent) {
+        let sDestination: any = oEvent.getSource().getBindingContext("capAppList")?.getObject();
         let oNav = (this.getOwnerComponent() as UIComponent)?.getRouter();
-        if (sDestination != undefined){
-            oNav.navTo(<string> sDestination);
+        if (sDestination != undefined && oNav.getRoute(sDestination.path) != undefined) {
+            oNav.navTo(<string>sDestination.path);
+            return;
         }
+
+        MessageToast.show("Application not yet implemented");
     }
 }
